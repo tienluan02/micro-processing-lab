8c4a05aff716d36bec301ab3985c784df753bf3f
tienluandev <nguyentienluan3@gmail.com>
2024-04-02 10:59:53 +0700

    finish ex1-lab2

---
+++ lab2/Lab2_NTLuan/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Apr 2, 2024
+ *      Author: NguyenTienLuan
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
